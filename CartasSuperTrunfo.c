744ad17a12bdced6781da623dc6f85d5e9883bed
Adrielrsantos <adrielrsantos@gmail.com>
2025-03-08 10:08:44 +0000

    Foram declaradas as variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,12 @@
 //Teste larissa
 
 int main() {
+    char cod_cidade;
+    char cidade[50];
+    int população, pontos_turisticos;
+    float area, pib;
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
